dfd8a0d327d0e394aae2abd8dbeeae9fdac3e4a6
Mandelamarle <89034948+Mandelamarle@users.noreply.github.com>
2023-11-16 11:44:53 +0300

    Add files via upload
    A program that adds two numbers and then returns a result to them.

---
+++ adds_Two_numbers.py
@@ -0,0 +1,6 @@
+"""First addition program"""
+
+first_Number = int(input("Enter your first number? "))
+second_Number =  int(input("Enter your second number? "))
+result = (first_Number) + (second_Number)
+print(result)
